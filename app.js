1fe6698514a93baa8036f73c07674b6298ca3095
Allie Zhao <allie.k.zhao@gmail.com>
2022-10-31 18:27:44 -0700

    js half done, added border to selected shell, added reset button to html

--- app.js
+++ app.js
@@ -6,24 +6,57 @@ const shellOneButton = document.getElementById('shellOneButton');
 const shellTwoButton = document.getElementById('shellTwoButton');
 const shellThreeButton = document.getElementById('shellThreeButton');
 const winCounter = document.getElementById('winCounter');
-const LoseCounter = document.getElementById('loseCounter');
+const loseCounter = document.getElementById('loseCounter');
 const gameCounter = document.getElementById('gameCounter');
 
 /* State */
+let wins = 0;
+let losses = 0;
 
 /* Events */
 shellOneButton.addEventListener('click', () => {
-	
+    shellOneButton.style.border = 'thick dotted lightblue';
+    const pearlLocation = setRandomPearlLocation(3);
+    console.log(pearlLocation);
+    let gameOutcome = 'lose'; // gameOutcome must be defined in this scope
+    if (pearlLocation === 1) {
+        gameOutcome = 'win';
+    } else {
+        gameOutcome = 'lose';
+    }
+    updateStats(gameOutcome);
+    displayResults(pearlLocation, 1, gameOutcome);
 });
+
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
 
-// updates game screen once user has clicked a shell: moves shell, displays pearl, highlights selected shell, changes header
-function displayResults(shellLocation, gameOutcome) {}
+// updates game screen once user has clicked a shell: moves shell, displays pearl, highlights selected shell, changes header, disables shell buttons
+function displayResults(pearlLocation, gameOutcome) {
+    shellOneButton.disabled = true;
+    shellTwoButton.disabled = true;
+    shellThreeButton.disabled = true;
+}
 
-// returns to initial game screen: resets shells, changes header
+// returns to initial game screen: resets shells, changes header, clears selected shell border indicator
 function resetGame() {}
 
-// update internall tracked stats as well as on-page ones
-function updateStats(gameOutcome) {}
+// update internal tracked stats as well as on-page ones
+function updateStats(gameOutcome) {
+    if (gameOutcome === 'win') {
+        wins++;
+        winCounter.textContent = wins;
+        console.log('win');
+    } else {
+        losses++;
+        loseCounter.textContent = losses;
+        console.log('lose');
+    }
+    gameCounter.textContent = wins + losses;
+}
+
+// sets random pearl location, returns a number between 1 and shellCount
+function setRandomPearlLocation(shellCount) {
+    return Math.ceil(Math.random() * shellCount);
+}
